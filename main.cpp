397be0f18663e75e52a3db7f29e3891ecfeaec5b
hugobbi <hgugobbi@gmail.com>
2022-10-05 21:31:10 -0300

    adicionando pesquisa por tags

--- main.cpp
+++ main.cpp
@@ -4,25 +4,30 @@
 using namespace std;
 using namespace aria::csv;
 
-hashtable<int,Player> players_table(100000);
-hashtable<int,vector<Avaliacao>> user_ratings_table(500000);
-hashtable<string,vector<int>> position_table(21);
+hashtable<int, Player> players_table(100000);
+hashtable<int, vector<Avaliacao>> user_ratings_table(500000);
+hashtable<string, vector<int>> position_table(21);
+hashtable<string, vector<int>> tags_table(937);
 
+vector<string> str_tokenizer(string s, char del);
+vector<int> removeDuplicates(const vector<int>& in_vector);
 void printPlayer(int id);
 void quickSort(int *arr, int low, int high);
 void quickSortAv(Avaliacao *arr, int low, int high);
 void user_search(int n);
 void position_search(int n, std::string pos);
-vector<string> str_tokenizer(string s, char del);
+void tags_search(std::vector<string> tags_to_be_searched);
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv) 
+{
     cout << "Reading names and inserting in trie tree..." << endl;
     Trie *TriePlayers = new Trie();
     std::ifstream fPlayer("players.csv");
     CsvParser parser(fPlayer);
 
     bool ignore_first = true;
-    for (auto& row : parser) {
+    for (auto& row : parser) 
+    {
         if(ignore_first)
         {
             ignore_first = false;
@@ -44,11 +49,12 @@ int main(int argc, char** argv) {
     }
 
     cout << "Reading ratings and inserting in hash table..." << endl;
-    std::ifstream fRating("minirating.csv");
+    std::ifstream fRating("rating.csv");
     CsvParser parser1(fRating);
 
     ignore_first = true;
-    for (auto& row : parser1) {
+    for (auto& row : parser1) 
+    {
         if(ignore_first)
         {
             ignore_first = false;
@@ -67,8 +73,7 @@ int main(int argc, char** argv) {
             temp->rating = temp->rating + (1.0f/i1)*(rating - temp->rating);
 
         temp->rcount += 1;
-        user_ratings_table.insere_array<Avaliacao>(user_id, {player_id, rating});
-        //user_ratings_table.insere_array(user_id, player_id);
+        user_ratings_table.insere_array<Avaliacao>(user_id, {(int)player_id, rating});
     }
 
     cout << "Reading tags and inserting in hash table..." << endl;
@@ -98,12 +103,12 @@ int main(int argc, char** argv) {
         if (!alreadyInserted)
             temp->tags.push_back(player_tag);
 
+        tags_table.insere_array<int>(player_tag, temp->fifa_id);
+
         /*cout << temp->name << " tags: ";
         for (string tt : temp->tags)
             cout << tt << ", ";
         cout << endl;*/
-
-        // INSERIR NA TABELA HASH
     }
 
     cout << "Done" << endl;
@@ -136,14 +141,16 @@ int main(int argc, char** argv) {
         else if (token == "tags")
         {
             // vetor de strings contendo tags
-            vector<string> tags = str_tokenizer(comando, '"');
-            
+            vector<string> tags = str_tokenizer(comando, '\'');
+
             // filtra espaços entre tags do vetor de strings para obter apenas as tags
             for (auto str = tags.begin(); str != tags.end(); str++)
             {
                 if (*str == " ")
                     tags.erase(str--);
             }
+
+            tags_search(tags);
         }
         else if(token == "exit")
             cout << "Exiting..." << endl;
@@ -152,6 +159,21 @@ int main(int argc, char** argv) {
     }
 
     return 0;
+}
+
+vector<string> str_tokenizer(string s, char del)
+{
+    vector<string> vector_s;
+    stringstream ss(s);
+    string word;
+    while (!ss.eof())
+    {
+        getline(ss, word, del);
+        if (!word.empty())
+            vector_s.push_back(word);
+    }
+
+    return vector_s;
 }
 
 void printPlayer(int id)
@@ -264,8 +286,8 @@ void position_search(int n, std::string pos)
         std::cout << "POSICAO NAO ENCONTRADA"  << std::endl;
         return;
     }
-    unsigned int vec_size = position_table[pos]->value.size();
-    unsigned int true_size;
+    int vec_size = position_table[pos]->value.size();
+    int true_size;
     if(vec_size > n)
         true_size = n;
     else
@@ -312,17 +334,50 @@ void position_search(int n, std::string pos)
         printPlayer(topN[i]);
 }
 
-vector<string> str_tokenizer(string s, char del)
+void tags_search(std::vector<string> tags_to_be_searched)
 {
-    vector<string> vector_s;
-    stringstream ss(s);
-    string word;
-    while (!ss.eof()) 
+    for (string tag_requested : tags_to_be_searched) // verifica se alguma tag passsada não possui jogadores na hash table
     {
-        getline(ss, word, del);
-        if (!word.empty())
-            vector_s.push_back(word);
+        if (tags_table[tag_requested]->value.empty())
+            cout << "No players found" << endl;
     }
 
-    return vector_s;
+    vector<int> players_with_tag = tags_table[tags_to_be_searched[0]]->value;
+    for (int i = 1; i < tags_to_be_searched.size(); i++)
+    {
+        for (auto playerid = players_with_tag.begin(); playerid != players_with_tag.end(); playerid++)
+        {
+            vector<string> player_tags = players_table[*playerid]->value.tags;
+            bool has_tag = false;
+            for (string player_tag : player_tags)
+            {
+                if (player_tag == tags_to_be_searched[i])
+                    has_tag = true;
+            }
+
+            if (!has_tag)
+                players_with_tag.erase(playerid--);
+        }
+    }
+
+    vector<int> filtered_players_with_tag = removeDuplicates(players_with_tag);
+    for (int filtered_player_id : filtered_players_with_tag)
+        printPlayer(filtered_player_id);
+}
+
+vector<int> removeDuplicates(const vector<int>& in_vector)
+{
+    vector<int> ids_filtered;
+    for (int id : in_vector) // remove duplicados
+    {
+        bool inserted = false;
+        for (int id_filtered : ids_filtered)
+        {
+            if (id == id_filtered)
+                inserted = true;
+        }
+        if (!inserted)
+            ids_filtered.push_back(id);
+    }
+    return ids_filtered;
 }
